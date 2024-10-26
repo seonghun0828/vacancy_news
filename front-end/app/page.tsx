7a2905c6b35fd0b3894533336b464ce2dc738389
seonghun <pch468@naver.com>
2024-10-26 15:54:18 +0900

    Feat: add news list template

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div></div>;
+import { WebsiteContent } from './types';
+
+export default async function Home() {
+  const { data }: { data: WebsiteContent[] } = await fetch(
+    'http://localhost:3000/api/news'
+  ).then((res) => res.json());
+
+  return (
+    <div>
+      {data.map(({ name, url }) => (
+        <div key={name}>
+          <h1>{name}</h1>
+          <a href={url} target="_blank">
+            {url}
+          </a>
+          <div>content</div>
+        </div>
+      ))}
+    </div>
+  );
 }
