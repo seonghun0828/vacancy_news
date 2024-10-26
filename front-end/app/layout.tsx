856e4797d91b4eb04da912fcadc9834de25cbd61
seonghun <pch468@naver.com>
2024-10-26 14:31:31 +0900

    first commit

---
+++ front-end/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata: Metadata = {
+  title: 'Vacancy News',
+  description: 'Vacancy News Website',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
